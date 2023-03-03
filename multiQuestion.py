42dd3dadb13814c3281731ae351ddbc933515bea
Rilwan <edunrilwan@gmail.com>
2023-03-03 22:53:45 +0100

    Add timer mechanism
    A timer mechanism was added as well as some other modifications

--- multiQuestion.py
+++ multiQuestion.py
@@ -1,12 +1,15 @@
 import pyttsx3
+import threading
 from Question import Question
 from datetime import datetime
-import time
 from time import *
+
 engine = pyttsx3.init()
+game_on = True
+
 # Array of questions to ask players
 question_prompt = [
-    "Where is Lambogini made? (a) Germany\n (b) Italy\n (c) Nigeria\n\n",
+    "Where is Lamborghini made? (a) Germany\n (b) Italy\n (c) Nigeria\n\n",
     "Who is the first man to travel to space? (a) Neil Armstrong\n (b) Yuri Gagarin\n (c) Micheal Jackson\n\n",
     "Who is the first man to land on the moon? (a) Neil Armstrong\n (b) Yuri Gagarin\n (c) Micheal Jackson\n\n",
     "Who is the founder of Apple Inc.? (a) Mark Zuckerberg\n (b) Steve Jobs\n (c) Sergery Brin\n",
@@ -17,6 +20,8 @@ question_prompt = [
     "Which of the following is a name of a continent? (a) South America\n (b) Niger\n (c) Spain\n\n",
     "_____ is the name of a continent and a country? (a) United States\n (b) Australia\n (c) Europe\n\n"
 ]
+
+
 # Array that contains instances of Question class and question prompt and the answer
 mult_question = [
     Question(question_prompt[0], "b"),
@@ -36,8 +41,10 @@ mult_question = [
 def run_test(questions):
     score = 0
     for each_question in questions:
-        engine.say(each_question.prompt)
-        engine.runAndWait()
+        if not game_on:
+            break
+        #engine.say(each_question.prompt)
+        #engine.runAndWait()
         answer = input(each_question.prompt)
         if answer == each_question.answer:
             score += 1
@@ -56,31 +63,93 @@ def run_test(questions):
         print("\nYour Grade: A")
 
 
+# Function to end quiz
+def end_quiz():
+    print('Time up')
+    global game_on
+    game_on = False
+    exit()
+
+
+# Function to check if the user input are integers
+def is_integer(num, token_number):
+    try:
+        if int(num) == token_number:
+            pass
+    except ValueError:
+        return False
+    else:
+        return True
+
+
 # pass our questions array into run_test function
+trials = 3
 now = datetime.now()
 current_time = now. strftime("%H:%M:%S %p")
 print("You started the CBT at ", current_time, "\n\n")
+
+
+# Ask for username
 name = str(input("Enter your name\n"))
 while not name:
     name = str(input("Please enter your name\n"))
 branch = str(input("Where is your branch?\n"))
+
+
+# Ask for branch
 while not branch:
     branch = str(input("Where is your branch?\n"))
-token_number = 7756
-num = int(input("Enter Test Token Number\n"))
-while not num:
-    num = int(input("Enter Test Token Number\n"))
-if num == token_number:
-    print("\nYou are eligible to take this test\n")
-else:
-    print("You are not allowed to take this test")
-    exit()
 
+
+# Ask for token number
+token_number = 7756
+num = input("Enter Test Token Number\n")
+
+
+# To check if the user entered any value
+while not num:
+    trials -= 1
+    print(f'You have {trials} trials left')
+    if trials == 0:
+        print('You cannot participate in this exam again. You exceeded maximum trials')
+        exit()
+    num = input("Enter Test Token Number\n")
+
+
+# To check if the user entered an integer or string
+while not is_integer(num, token_number):
+    trials -= 1
+    print(f'Token number must contain only integers. You have {trials} trials left')
+    if trials == 0:
+        print('You cannot participate in this exam again. You exceeded maximum trials')
+        exit()
+    num = input("Enter Test Token Number\n")
+    if is_integer(num, token_number):
+        break
+
+
+# To check if user input is equal to the token number
+while int(num) != token_number:
+    trials -= 1
+    print(f'You entered the wrong token number. {trials} trials left')
+    if trials == 0:
+        print('You cannot participate in this exam again. You exceeded maximum trials')
+        exit()
+    num = input("Enter Test Token Number\n")
+
+
+# Begin Timer
+timer = threading.Timer(20.0, end_quiz)
+timer.start()
 start = time()
+
+
+# Run Application
 run_test(mult_question)
+timer.cancel()
 stop = time()
 end = datetime.now()
 end_time = end.strftime("%H:%M:%S %p")
 print("\n\nYou ended the test at ", end_time)
 print("You used ", stop - start, " seconds.")
-input()
+
